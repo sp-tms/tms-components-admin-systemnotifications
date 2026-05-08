047b7565e57895ad52e04f31ae1b4709b2fad6fc
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:51:58 +0530

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Tms\Components\System\Notifications;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
